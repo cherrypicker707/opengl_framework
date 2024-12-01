328e66390d65ee3e143e47287a9ae35f89c8653e
Wiktor Marczak <cherrypicker707@proton.me>
2024-12-01 14:04:19 +0100

    GLFW and OpenGL added.

--- source/main.c
+++ source/main.c
@@ -1,7 +1,22 @@
-#include <stdio.h>
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+
+#include <assert.h>
 
 int main()
 {
-    printf("Hello World!\n");
+    assert(glfwInit());
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "Test Program", NULL, NULL);
+    assert(window);
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
