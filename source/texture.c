7be87f060e29d35a4670f58f907a73543a91b1f9
Wiktor Marczak <cherrypicker707@proton.me>
2025-01-06 12:10:49 +0100

    Circle texture creating fixed.

--- source/texture.c
+++ source/texture.c
@@ -4,12 +4,13 @@
 unsigned int textureCreateCircle(unsigned int width)
 {
     unsigned int pixelCount = width * width;
-    unsigned char data[pixelCount * 1];
+    unsigned char data[4 * pixelCount];
 
-    for (int index = 0; index < pixelCount; index++)
+    for (int index = 0; index < 4 * pixelCount; index++)
     {
-        unsigned int row = index / width;
-        unsigned int column = index % width;
+        unsigned int pixelIndex = index / 4;
+        unsigned int row = pixelIndex / width;
+        unsigned int column = pixelIndex % width;
         unsigned int rowOffset = row - width / 2;
         unsigned int columnOffset = column - width / 2;
         unsigned int distanceToOriginSquared = rowOffset * rowOffset + columnOffset * columnOffset;
@@ -31,7 +32,7 @@ unsigned int textureCreateCircle(unsigned int width)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, width, width, 0, GL_RED, GL_UNSIGNED_BYTE, data);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, width, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
 
     return texture;
